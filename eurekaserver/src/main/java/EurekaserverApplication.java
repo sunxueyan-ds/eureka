5d590eba7a8e8e32efa54534579a4fff8cc585ac
sunxueyan-ds <sunxueyan-ds@gome.com.cn>
2018-07-05 09:14:41 +0800

    1234

---
+++ eurekaserver/src/main/java/EurekaserverApplication.java
@@ -0,0 +1,18 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author sunxueyan-ds
+ * @Title: EurekaserverApplication
+ * @ProjectName eurekaserver
+ * @Description: TODO
+ * @date 2018/7/4 14:23
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication {
+    public static void main(String[] args){
+        SpringApplication.run(EurekaserverApplication.class,args);
+    }
+}
